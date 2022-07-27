bde0786abeb22f0bd6be7a0f898c5ddb93f56c3d
alexanders51 <alexanton@tutanota.com>
2022-07-27 19:56:46 +0300

    Initial support for skipping to SearchScreenActivity when user prefs are stored

--- app/src/main/java/com/practica/movieapp/ui/OnboardingScreenActivity.kt
+++ app/src/main/java/com/practica/movieapp/ui/OnboardingScreenActivity.kt
@@ -7,8 +7,13 @@ import android.view.Window
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.practica.movieapp.R
+import com.practica.movieapp.data.RemoteDataRetriever
 import com.practica.movieapp.ui.actors.ActorsScreenActivity
 import com.practica.movieapp.ui.genres.GenresScreenActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import kotlin.properties.Delegates
 
 class OnboardingScreenActivity : AppCompatActivity() {
@@ -25,8 +30,20 @@ class OnboardingScreenActivity : AppCompatActivity() {
         window.requestFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
-
         setClickListeners()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        GlobalScope.launch(Dispatchers.IO) {
+            val flag = RemoteDataRetriever.userPreferencesExist()
+            withContext(Dispatchers.Main) {
+                if (flag) {
+                    SearchScreenActivity.open(this@OnboardingScreenActivity)
+                    finish()
+                }
+            }
+        }
     }
 
     private fun setClickListeners() {

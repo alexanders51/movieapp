397cf3e9ba210fc6368d19d307f65db7b861fbe0
alexanders51 <alexanton@tutanota.com>
2022-07-30 15:05:56 +0300

    Fixing GlobalScope warnings in SearchFragment

--- app/src/main/java/com/practica/movieapp/ui/searchui/search/SearchFragment.kt
+++ app/src/main/java/com/practica/movieapp/ui/searchui/search/SearchFragment.kt
@@ -1,39 +1,40 @@
 package com.practica.movieapp.ui.searchui.search
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.practica.movieapp.R
 import com.practica.movieapp.data.RemoteDataRetriever
 import com.practica.movieapp.data.movies.Movie
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
-
     private var movies : List<Movie> = emptyList()
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+    private val mainDispatcher: MainCoroutineDispatcher = Dispatchers.Main
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        GlobalScope.launch (Dispatchers.IO) {
+        CoroutineScope(ioDispatcher).launch {
             RemoteDataRetriever.updateMovies()
             movies = RemoteDataRetriever.getPreloadedMovies()
-            withContext(Dispatchers.Main) {
+            withContext(mainDispatcher) {
                 setupRecyclerView(view)
             }
         }
     }
 
     private fun setupRecyclerView(view: View) {
-        val rvItems = view.findViewById<RecyclerView>(R.id.rvMovieItems)
-        rvItems.layoutManager = LinearLayoutManager(view.context, LinearLayoutManager.VERTICAL, false)
-        rvItems.adapter = SearchMoviesAdapter(movies)
+        val llm = LinearLayoutManager(view.context)
+        val rv = view.findViewById<RecyclerView>(R.id.rvMovieItems)
+
+        llm.orientation = LinearLayoutManager.VERTICAL
+        llm.reverseLayout = false
+
+        rv.layoutManager = llm
+        rv.adapter = MoviesAdapter(movies)
     }
 }

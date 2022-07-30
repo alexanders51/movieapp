cd0ebb4ffdee582b36509532514e46c074379a0f
alexanders51 <alexanton@tutanota.com>
2022-07-30 15:09:10 +0300

    ImageDownloadManager in App.kt

--- app/src/main/java/com/practica/movieapp/app/App.kt
+++ app/src/main/java/com/practica/movieapp/app/App.kt
@@ -1,15 +1,15 @@
 package com.practica.movieapp.app
 
 import android.app.Application
+import com.practica.movieapp.data.ImageDownloadManager
 import com.practica.movieapp.data.RemoteDataRetriever
 import com.practica.movieapp.database.Database
-import com.practica.movieapp.ui.actors.ActorsImageRetriever
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         Database.instance.initialize(this)
-        ActorsImageRetriever.initialize()
+        ImageDownloadManager.initialize()
         RemoteDataRetriever.initialize()
     }
 }

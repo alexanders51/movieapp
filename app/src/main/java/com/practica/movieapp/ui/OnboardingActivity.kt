8f40b68c3e7bcaeb5719d5e0224b4d3c1c4041bf
alexanders51 <alexanton@tutanota.com>
2022-08-01 01:26:05 +0300

    UI tweaks, user flow changed: onboarding fetches local data and receives data returned from activities

--- app/src/main/java/com/practica/movieapp/ui/OnboardingActivity.kt
+++ app/src/main/java/com/practica/movieapp/ui/OnboardingActivity.kt
@@ -3,18 +3,32 @@ package com.practica.movieapp.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.view.Window
 import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.launch
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.practica.movieapp.R
-import com.practica.movieapp.data.RemoteDataRetriever
-import com.practica.movieapp.ui.actors.ActorsActivity
-import com.practica.movieapp.ui.genres.GenresActivity
-import kotlinx.coroutines.*
+import com.practica.movieapp.data.DataHandler
+import com.practica.movieapp.ui.actors.ActorsActivityContract
+import com.practica.movieapp.ui.actors.ActorsReturnData
+import com.practica.movieapp.ui.genres.GenresActivityContract
+import com.practica.movieapp.ui.genres.GenresReturnData
 
 class OnboardingActivity : AppCompatActivity() {
-    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
-    private val mainDispatcher: MainCoroutineDispatcher = Dispatchers.Main
+    private var actorsRet: ActorsReturnData? = null
+    private var genresRet: GenresReturnData? = null
+
+    private val actorsActivityCustomLauncher = registerForActivityResult(ActorsActivityContract()) {
+        if (it != null) actorsRet = it
+    }
+
+    private val genresActivityCustomLauncher = registerForActivityResult(GenresActivityContract()) {
+        if (it != null) genresRet = it
+    }
 
     companion object {
         fun open(context: Context) {
@@ -25,34 +39,75 @@ class OnboardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         window.requestFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
+
+        fetchLocalData()
         setContentView(R.layout.activity_onboarding)
         setClickListeners()
     }
 
     override fun onResume() {
         super.onResume()
-        CoroutineScope(ioDispatcher).launch {
-            val flag = RemoteDataRetriever.userPreferencesExist()
-            if (flag) RemoteDataRetriever.updateMovies()
-            withContext(mainDispatcher) {
-                if (flag) {
-                    MainActivity.open(this@OnboardingActivity)
-                    finish()
-                }
-            }
-        }
+        updateUiElements()
     }
 
     private fun setClickListeners() {
         val btnActors = findViewById<Button>(R.id.btnActors)
         val btnGenres = findViewById<Button>(R.id.btnGenres)
+        val fabNext = findViewById<FloatingActionButton>(R.id.fabOnboardingNext)
 
         btnGenres.setOnClickListener {
-            GenresActivity.open(this)
+            genresActivityCustomLauncher.launch()
         }
 
         btnActors.setOnClickListener {
-            ActorsActivity.open(this)
+            actorsActivityCustomLauncher.launch()
         }
+
+        fabNext.setOnClickListener {
+            MainActivity.open(this)
+            finish()
+        }
+    }
+
+    private fun updateUiElements() {
+        val flagGenre = if (genresRet != null) genresRet?.nrSelectedGenres!! > 0 else false
+        val flagActor = if (actorsRet != null) actorsRet?.nrSelectedActors!! > 0 else false
+        val btnGenres = findViewById<Button>(R.id.btnGenres)
+        val btnActors = findViewById<Button>(R.id.btnActors)
+        val ivGenreCheck = findViewById<ImageView>(R.id.ivCheckGenres)
+        val ivActorCheck = findViewById<ImageView>(R.id.ivCheckActors)
+
+        updateControlDecoration(btnGenres, ivGenreCheck, flagGenre)
+        updateControlDecoration(btnActors, ivActorCheck, flagActor)
+        updateFloatingButton(flagGenre && flagActor)
+    }
+
+    private fun updateControlDecoration(btn: Button, iv: ImageView, flag: Boolean) {
+        if (flag) {
+            btn.setBackgroundColor(ContextCompat.getColor(this, R.color.magenta_300))
+            btn.setTextColor(ContextCompat.getColor(this, R.color.black))
+            iv.visibility = View.VISIBLE
+        }
+        else {
+            btn.setBackgroundColor(ContextCompat.getColor(this, R.color.magenta_500_30a))
+            btn.setTextColor(ContextCompat.getColor(this, R.color.white))
+            iv.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun updateFloatingButton(flag: Boolean) {
+        val fab = findViewById<FloatingActionButton>(R.id.fabOnboardingNext)
+        fab.visibility = when (flag) {
+            true -> View.VISIBLE
+            else -> View.INVISIBLE
+        }
+    }
+
+    private fun fetchLocalData() {
+        val selActors = DataHandler.getLocalActors().filter { it.isSelected }.map{ it.name }
+        val selGenres = DataHandler.getLocalGenres().filter { it.isSelected }.map{ it.name }
+
+        actorsRet = ActorsReturnData(selActors.size, selActors.toTypedArray())
+        genresRet = GenresReturnData(selGenres.size, selGenres.toTypedArray())
     }
 }

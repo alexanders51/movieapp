7ee6a52595be05450ee472ac462854c255d905c4
alexanders51 <alexanton@tutanota.com>
2022-08-01 00:50:03 +0300

    Implementing a custom GlideApp module

---
+++ app/src/main/java/com/practica/movieapp/data/CustomGlideApp.kt
@@ -0,0 +1,18 @@
+package com.practica.movieapp.data
+
+import android.annotation.SuppressLint
+import android.content.Context
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestOptions
+
+@GlideModule
+class CustomGlideApp : AppGlideModule() {
+    @SuppressLint("CheckResult")
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        super.applyOptions(context, builder)
+        builder.apply { RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL) }
+    }
+}
--- app/src/main/java/com/practica/movieapp/data/ImageDownloadManager.kt
+++ app/src/main/java/com/practica/movieapp/data/ImageDownloadManager.kt
@@ -1,21 +1,15 @@
 package com.practica.movieapp.data
 
 import android.content.Context
-import android.os.Build
 import android.widget.ImageView
-import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
-import com.bumptech.glide.Glide
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
 import com.practica.movieapp.R
 import com.practica.movieapp.data.configuration.ConfigurationImagesResponse
 import com.practica.movieapp.data.configuration.ConfigurationRetriever
 import com.practica.movieapp.network.ApiClient
 import kotlinx.coroutines.*
 
-@GlideModule
-object ImageDownloadManager : AppGlideModule() {
+object ImageDownloadManager {
     private lateinit var configurationImagesResponse: ConfigurationImagesResponse
     private var configurationRetriever: ConfigurationRetriever = ConfigurationRetriever(ApiClient.instance.retrofit!!)
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
@@ -29,7 +23,8 @@ object ImageDownloadManager : AppGlideModule() {
         }
     }
 
-    fun downloadH632ImageWithPath(context: Context?, path: String?, iv: ImageView?) = downloadImageWithPath(context, path, iv, imageProfileSizes[2])
+
+    fun downloadH632ImageWithPath(context: Context, path: String?, iv: ImageView) = downloadImageWithPath(context, path, iv, imageProfileSizes[2])
 
     private fun retrieveConfiguration() {
         configurationImagesResponse = configurationRetriever.getConfig().images
@@ -37,14 +32,14 @@ object ImageDownloadManager : AppGlideModule() {
         imageProfileSizes = configurationImagesResponse.profileSizes
     }
 
-    private fun downloadImageWithPath(context: Context?, path: String?, iv: ImageView?, size: String) {
+    private fun downloadImageWithPath(context: Context, path: String?, iv: ImageView, size: String) {
         if (path == null) {
-            iv?.setImageResource(R.drawable.ic_no_image)
-            iv?.setColorFilter(ContextCompat.getColor(context!!, R.color.violet_300))
-            iv?.setBackgroundColor(ContextCompat.getColor(context!!, R.color.violet_200_30a))
+            iv.setImageResource(R.drawable.ic_no_image)
+            iv.setColorFilter(ContextCompat.getColor(context, R.color.violet_300))
+            iv.setBackgroundColor(ContextCompat.getColor(context, R.color.violet_200_30a))
         }
         else {
-            Glide.with(context!!).load(imageBaseUrl + size + path).into(iv!!)
+            GlideApp.with(context).load(imageBaseUrl + size + path).into(iv)
         }
     }
 

e3be7a41e6a3738c8b380ce3f543fcc56e06d3d8
alexanders51 <alexanton@tutanota.com>
2022-08-01 22:38:52 +0300

    Adapting MovieMapper for new Movie entity

--- app/src/main/java/com/practica/movieapp/data/movies/MovieMapper.kt
+++ app/src/main/java/com/practica/movieapp/data/movies/MovieMapper.kt
@@ -3,36 +3,14 @@ package com.practica.movieapp.data.movies
 class MovieMapper {
     fun map(response: MovieResponse): Movie = Movie(
         id = response.id,
-        originalTitle = response.originalTitle,
-        title = response.title,
-        overview = response.overview,
-        releaseDate = response.releaseDate,
-        adult = response.adult,
-        posterPath = response.posterPath,
-        backdropPath = response.backdropPath
-    )
-
-    fun mapFavorite(response: MovieResponse): MovieFavorite = MovieFavorite(
-        id = response.id,
-        originalTitle = response.originalTitle,
-        title = response.title,
-        overview = response.overview,
-        releaseDate = response.releaseDate,
-        adult = response.adult,
-        posterPath = response.posterPath,
-        backdropPath = response.backdropPath,
-        isFavorite = false
-    )
-
-    fun mapWatched(response: MovieResponse): MovieWatched = MovieWatched(
-        id = response.id,
-        originalTitle = response.originalTitle,
-        title = response.title,
-        overview = response.overview,
-        releaseDate = response.releaseDate,
-        adult = response.adult,
-        posterPath = response.posterPath,
-        backdropPath = response.backdropPath,
-        isWatched = false
+        originalTitle  = response.originalTitle,
+        title          = response.title,
+        overview       = response.overview,
+        releaseDate    = response.releaseDate,
+        adult          = response.adult,
+        posterPath     = response.posterPath,
+        backdropPath   = response.backdropPath,
+        isFavorite     = false,
+        isWatched      = false
     )
 }

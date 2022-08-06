0f774de2fe16d4ce8550fcd8a77c8856e683b121
alexanders51 <alexanton@tutanota.com>
2022-08-06 21:40:44 +0300

    Adding a new constant for MovieDetails api (youtube video retrieval)

--- app/src/main/java/com/practica/movieapp/utils/Constants.kt
+++ app/src/main/java/com/practica/movieapp/utils/Constants.kt
@@ -4,4 +4,5 @@ object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val API_KEY = "0664398416ef91713108b4a56c86a754"
     const val LANGUAGE = "en-US"
+    const val APPEND_TO_RESPONSE = "videos"
 }

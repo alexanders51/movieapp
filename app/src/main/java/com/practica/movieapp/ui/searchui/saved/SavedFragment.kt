1488d4ac399ea96b0ca00c6bb4cff3c06aa776f2
alexanders51 <alexanton@tutanota.com>
2022-08-06 21:56:48 +0300

    SavedFragment uses binding now

--- app/src/main/java/com/practica/movieapp/ui/searchui/saved/SavedFragment.kt
+++ app/src/main/java/com/practica/movieapp/ui/searchui/saved/SavedFragment.kt
@@ -1,69 +1,73 @@
 package com.practica.movieapp.ui.searchui.saved
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.practica.movieapp.R
-import com.practica.movieapp.data.DataHandler
-import com.practica.movieapp.data.movies.Movie
-import com.practica.movieapp.ui.searchui.saved.tabs.AdapterTabPager
+import com.practica.movieapp.databinding.FragmentSavedBinding
+import com.practica.movieapp.ui.searchui.saved.tabs.SavedViewPagerAdapter
 import com.practica.movieapp.ui.searchui.saved.tabs.FavoriteFragment
 import com.practica.movieapp.ui.searchui.saved.tabs.WatchedFragment
-import kotlinx.coroutines.*
 
 
-class SavedFragment : Fragment(R.layout.fragment_saved) {
+class SavedFragment : Fragment() {
     private val tabTitles = arrayOf(
         R.string.tab_text_favorite,
         R.string.tab_text_watched
     )
 
-    private var movies: ArrayList<Movie> = ArrayList()
+    private lateinit var _binding: FragmentSavedBinding
+    private val binding get() = _binding
 
-    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
-    private val mainDispatcher: MainCoroutineDispatcher = Dispatchers.Main
+    private lateinit var tabs: TabLayout
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var savedViewPagerAdapter: SavedViewPagerAdapter
 
-    private var adapterTabPager: AdapterTabPager? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSavedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val viewPager = view.findViewById<ViewPager2>(R.id.ftViewPager)
-        val tabs: TabLayout = view.findViewById(R.id.ftTabs)
+        with (binding) {
+            tabs = this.ftFsaTabs
+            viewPager2 = this.vp2FsaViewPager
 
-        refreshAdapter(tabs, viewPager)
-
+            refreshAdapter(tabs, viewPager2)
+        }
     }
 
     override fun onResume() {
         super.onResume()
-
-        val viewPager = view?.findViewById<ViewPager2>(R.id.ftViewPager)
-        val tabs: TabLayout? = view?.findViewById(R.id.ftTabs)
-
-        refreshAdapter(tabs!!, viewPager!!)
+        refreshAdapter(tabs, viewPager2)
     }
 
-    private fun refreshAdapter(tabs: TabLayout, viewPager: ViewPager2) {
-        CoroutineScope(ioDispatcher).launch {
-            DataHandler.updateLocal()
-            withContext(mainDispatcher) {
-                movies = ArrayList(DataHandler.getLocalMovies())
+    private fun refreshAdapter(tabs: TabLayout, viewPager2: ViewPager2) {
+        savedViewPagerAdapter = SavedViewPagerAdapter(requireActivity())
 
-                adapterTabPager = activity?.let { AdapterTabPager(it) }
-
-                adapterTabPager?.addFragment(FavoriteFragment(movies), getString(tabTitles[0]))
-                adapterTabPager?.addFragment(WatchedFragment(movies), getString(tabTitles[1]))
-
-                viewPager.adapter = adapterTabPager!!
-                viewPager.currentItem = 0
-                TabLayoutMediator(tabs, viewPager) { tab, position ->
-                    tab.text = adapterTabPager?.getPageTitle(position)
-                }.attach()
+        with(savedViewPagerAdapter) {
+            with(tabTitles) {
+                addFragment(FavoriteFragment(), getString(this[0]))
+                addFragment(WatchedFragment(), getString(this[1]))
             }
+
+            viewPager2.adapter = savedViewPagerAdapter
+            viewPager2.currentItem = 0
+
+            TabLayoutMediator(tabs, viewPager2) { tab, position ->
+                tab.text = this.getPageTitle(position)
+            }.attach()
         }
     }
 }

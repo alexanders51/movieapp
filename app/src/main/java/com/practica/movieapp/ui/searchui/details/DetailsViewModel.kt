c95e75c4d38269f6d77e30e96e59c5f0344185e0
alexanders51 <alexanton@tutanota.com>
2022-08-06 21:55:01 +0300

    Adding new fields in DetailsViewModel

--- app/src/main/java/com/practica/movieapp/ui/searchui/details/DetailsViewModel.kt
+++ app/src/main/java/com/practica/movieapp/ui/searchui/details/DetailsViewModel.kt
@@ -3,13 +3,12 @@ package com.practica.movieapp.ui.searchui.details
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.practica.movieapp.data.movies.Movie
+import com.practica.movieapp.data.movies.MovieDetails
+import com.practica.movieapp.data.movies.get.MoviesRepository
 
 class DetailsViewModel : ViewModel() {
-    private val currentMovie = MutableLiveData<Movie?>()
-
-    fun getCurrentMovie() = currentMovie
-
-    fun setCurrentMovie(movie: Movie?) {
-        currentMovie.value = movie
-    }
+    val movieDetails = MutableLiveData<MovieDetails>()
+    val movieObject = MutableLiveData<Movie>()
+    val favorite = MutableLiveData<Boolean>()
+    val watched = MutableLiveData<Boolean>()
 }

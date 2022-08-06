b41605a9ed0ff6fe8901b666c6152c0f94aa1f32
alexanders51 <alexanton@tutanota.com>
2022-08-06 21:47:45 +0300

    movie/{movie_id} API support

--- app/src/main/java/com/practica/movieapp/data/movies/get/MoviesApiService.kt
+++ app/src/main/java/com/practica/movieapp/data/movies/get/MoviesApiService.kt
@@ -1,8 +1,10 @@
 package com.practica.movieapp.data.movies.get
 
+import com.practica.movieapp.data.movies.MovieDetailsResponse
 import com.practica.movieapp.data.movies.MoviesPageResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MoviesApiService {
@@ -22,4 +24,12 @@ interface MoviesApiService {
         @Query("page") page: String,
         @Query("query") query: String
     ): Call<MoviesPageResponse>
+
+    @GET("movie/{movie_id}")
+    fun getMovieDetailsById(
+        @Path("movie_id") movieId: String,
+        @Query("api_key") apiKey: String,
+        @Query("language") lang: String,
+        @Query("append_to_response") appendToResponse: String
+    ): Call<MovieDetailsResponse>
 }
